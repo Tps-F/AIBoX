e66059622bce91e39d76e3a5c9944c04610c7a60
Ftps <63702646+Tps-F@users.noreply.github.com>
2023-05-14 12:40:45 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config')
-const nextConfig = module.exports = {
+const nextConfig = {
   i18n,
+  pagesDir: 'pages'
   }
 
 module.exports = nextConfig
